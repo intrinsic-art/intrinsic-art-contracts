cfe203c1546665753fc3e9f81f7ee2750cbb39b3
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-13 10:07:40 -0400

    Created safe minting w/ easy counting

--- contracts/MockElement.sol
+++ contracts/MockElement.sol
@@ -3,13 +3,24 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "./interfaces/IMockElement.sol";
 
 contract MockElement is IMockElement, Initializable, ERC721Upgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
     function initialize(string calldata name, string calldata symbol)
         external
         initializer
     {
         __ERC721_init(name, symbol);
+    }
+
+    function safeMint(address to) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
     }
 }
