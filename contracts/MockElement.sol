2c19ab9d14e8dd81308d8b7237ffcadf9ee31893
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-13 10:03:20 -0400

    added interfaces to mock elment

--- contracts/MockElement.sol
+++ contracts/MockElement.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./interfaces/IMockElement.sol";
 
-contract MyToken is Initializable, ERC721Upgradeable {
+contract MockElement is IMockElement, Initializable, ERC721Upgradeable {
     function initialize(string calldata name, string calldata symbol)
         external
         initializer
