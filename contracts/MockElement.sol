7f3c4696bfe4c88b003d61e1f3abacd410e27301
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-13 10:39:07 -0400

    added tokenURI to erc721

--- contracts/MockElement.sol
+++ contracts/MockElement.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
@@ -11,22 +12,44 @@ contract MockElement is
     IMockElement,
     Initializable,
     ERC721Upgradeable,
+    ERC721URIStorageUpgradeable,
     ERC721BurnableUpgradeable
 {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
 
-    function initialize(string calldata name, string calldata symbol)
+    function initialize(string memory name, string memory symbol)
         external
         initializer
     {
         __ERC721_init(name, symbol);
+        __ERC721URIStorage_init();
+        __ERC721Burnable_init();
     }
 
-    function safeMint(address to) public {
+    function safeMint(address to, string memory uri) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
