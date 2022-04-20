5f6044d6cdc196b76dfd8951404862fc0320c9c7
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-19 23:27:48 -0400

    deploy scripts

--- deploy/001_deploy_mockNFT.ts
+++ deploy/001_deploy_mockNFT.ts
@@ -1,9 +1,23 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { deployContract } from "../helpers/deployContract";
+import Config from "../helpers/config";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  await deployContract(hre, "MockNFT", []);
+  await deployContract(hre, "MockCanvas", []);
+
+  const mockCanvas = await hre.ethers.getContract("MockCanvas");
+  await mockCanvas.initialize();
+  await mockCanvas.addProject(
+    Config.AddProject.projectName,
+    Config.AddProject.artistAddress, // Artist Address
+    Config.AddProject.invocations,
+    Config.AddProject.dynamic
+  );
+
+  for (let i = 0; i < 10; i++) {
+    mockCanvas.safeMint(Config.safeMint.to, Config.safeMint.projectId);
+  }
 };
 
 export default func;
