32d977b25a6535b47b6c8a4acbaba88eafb47d52
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-06 11:09:38 -0400

    wrapping with propoer data structures

--- contracts/canvas/MockCanvasWrapping.sol
+++ contracts/canvas/MockCanvasWrapping.sol
@@ -7,19 +7,31 @@ import "../interfaces/IMockElement.sol";
 contract MockCanvasWrapping is Initializable {
     IMockElement public mockElement;
 
-    // canvasContract => canvasId => element[]
-    mapping(address => mapping(uint256 => uint256[])) public canvasIdToElements;
+    // canvasId => elementBalances
+    mapping(uint256 => mapping(uint256 => uint256)) public balances;
+    // canvasId => element[]
+    mapping(uint256 => uint256[]) public canvasIdToElements;
+    // projectId => featureSet / features
+    mapping(uint256 => string[][]) public projectIdToFeatures;
 
     function __Wrap_init(address _mockElement) internal onlyInitializing {
         mockElement = IMockElement(_mockElement);
     }
 
-    // todo: propoer amount handling
     function wrap(
         address owner,
         uint256[] memory elementIds,
-        uint256[] memory amounts
+        uint256[] memory amounts,
+        uint256 canvasId
     ) public {
+        require(elementIds.length == amounts.length);
+        for (uint256 i; i < elementIds.length; i++) {
+            if (balances[canvasId][elementIds[i]] > 0) {
+              continue;
+            }
+            canvasIdToElements[canvasId].push(elementIds[i]);
+            balances[canvasId][elementIds[i]] += amounts[i];
+        }
         mockElement.safeBatchTransferFrom(
             owner,
             address(this),
