4040b08d10fb3ab79fbd5a04ef8a5d623dce8db8
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-07 13:19:02 -0400

    updated nomenclature and utilized better method

--- contracts/canvas/MockCanvasWrapping.sol
+++ contracts/canvas/MockCanvasWrapping.sol
@@ -9,54 +9,62 @@ import "../interfaces/IMockElement.sol";
 contract MockCanvasWrapping is Initializable {
     IMockElement public mockElement;
 
-    // canvasId => elementBalances
-    mapping(uint256 => mapping(uint256 => uint256)) public balances;
-    // canvasId => element[]
-    mapping(uint256 => uint256[]) public canvasIdToElements;
-    // cavasId => element => arrayIndex
     mapping(uint256 => mapping(uint256 => uint256))
-        public canvasIdToElementIndex;
+        public canvasIdToFeatureToBalances;
+    mapping(uint256 => uint256[]) public canvasIdToFeatures;
+    mapping(uint256 => mapping(uint256 => uint256))
+        public canvasIdToFeatureArrayIndex;
+    mapping(uint256 => mapping(string => uint256))
+        public canvasIdToCategoryToFeatureId;
 
-    // We need to know if a category is currently being used and what is it being used with
-    // canvasId => Category => elemenetId
-    mapping(uint256 => mapping(string => uint256)) public canvasIdToCategoryToelementId; 
-
-    // checking categories
     function __Wrap_init(address _mockElement) internal onlyInitializing {
         mockElement = IMockElement(_mockElement);
     }
 
     function wrap(
         address owner,
-        uint256[] memory elementIds,
+        uint256[] memory featureIds,
         uint256[] memory amounts,
         uint256 canvasId
     ) public {
-        require(elementIds.length == amounts.length);
-        for (uint256 i; i < elementIds.length; i++) {
-            if (balances[canvasId][elementIds[i]] > 0) {
-                continue;
-            }
+        for (uint256 i; i < featureIds.length; i++) {
+            string memory featureCategory = mockElement.findIdToCategory(
+                address(this),
+                featureIds[i]
+            );
+            // If the assigned feature == current feature then just update balance
+            if (
+                canvasIdToCategoryToFeatureId[canvasId][featureCategory] !=
+                featureIds[i]
+            ) {
+                // If there is a feature assigned to a category
+                // Move on to the next index, Do not wrap features w/ same category
+                if (
+                    canvasIdToCategoryToFeatureId[canvasId][featureCategory] > 0
+                ) {
+                    continue;
+                }
 
-            // todo:seperate functions for readability
-            // Stop future features with same category
-            string memory featureCategory = mockElement.findIdToCategory(address(this), elementIds[i]); 
-            if(canvasIdToCategoryToelementId[canvasId][featureCategory] == 0) {
-              continue;
-            }
-            canvasIdToCategoryToelementId[canvasId][featureCategory] = elementIds[i];
+                // Assign a feature to a category
+                canvasIdToCategoryToFeatureId[canvasId][
+                    featureCategory
+                ] = featureIds[i];
 
-            // update storage
-            canvasIdToElementIndex[canvasId][
-                elementIds[i]
-            ] = canvasIdToElements[canvasId].length;
-            canvasIdToElements[canvasId].push(elementIds[i]);
-            balances[canvasId][elementIds[i]] += amounts[i];
+                // Push the feature to the array of features/canvasId
+                canvasIdToFeatures[canvasId].push(featureIds[i]);
+
+                // Track the current index for easy lookup
+                canvasIdToFeatureArrayIndex[canvasId][featureIds[i]] =
+                    canvasIdToFeatures[canvasId].length -
+                    1;
+            }
+            // update balances of features
+            canvasIdToFeatureToBalances[canvasId][featureIds[i]] += amounts[i];
         }
         mockElement.safeBatchTransferFrom(
             owner,
             address(this),
-            elementIds,
+            featureIds,
             amounts,
             ""
         );
@@ -64,36 +72,41 @@ contract MockCanvasWrapping is Initializable {
 
     function unWrap(
         address receiver,
-        uint256[] memory elementIds,
+        uint256[] memory featureIds,
         uint256[] memory amounts,
         uint256 canvasId
     ) public {
-        require(elementIds.length == amounts.length);
-        for (uint256 i; i < elementIds.length; i++) {
-            if (balances[canvasId][elementIds[i]] == 0) {
+        for (uint256 i; i < featureIds.length; i++) {
+            // If there is not a balance/ a feature is not wrapped - continue
+            if (canvasIdToFeatureToBalances[canvasId][featureIds[i]] == 0) {
                 continue;
             }
 
-            // set featureId to zero when unwrapping
-            string memory featureCategory = mockElement.findIdToCategory(address(this), elementIds[i]); 
-            canvasIdToCategoryToelementId[canvasId][featureCategory] = 0;
+            canvasIdToFeatureToBalances[canvasId][featureIds[i]] -= amounts[i];
+            // if the canvas balance == 0 then remove it
+            if (canvasIdToFeatureToBalances[canvasId][featureIds[i]] == 0) {
+                uint256 featureIndex = canvasIdToFeatureArrayIndex[canvasId][
+                    featureIds[i]
+                ];
+                uint256 arrayLength = canvasIdToFeatures[canvasId].length;
+                // Save the last index at the current index being removed
+                canvasIdToFeatures[canvasId][featureIndex] = canvasIdToFeatures[
+                    canvasId
+                ][arrayLength - 1];
+                canvasIdToFeatures[canvasId].pop();
 
-            uint256 arrayLength = canvasIdToElements[canvasId].length;
-            uint256 elementIndex = canvasIdToElementIndex[canvasId][
-                elementIds[i]
-            ];
-            uint256 lastElementId = canvasIdToElements[canvasId][
-                arrayLength - 1
-            ];
-
-            canvasIdToElements[canvasId][elementIndex] = lastElementId;
-            canvasIdToElements[canvasId].pop();
-            balances[canvasId][elementIds[i]] -= amounts[i];
+                // assign the 0 index to the current category
+                string memory featureCategory = mockElement.findIdToCategory(
+                    address(this),
+                    featureIds[i]
+                );
+                canvasIdToCategoryToFeatureId[canvasId][featureCategory] = 0;
+            }
         }
         mockElement.safeBatchTransferFrom(
             address(this),
             receiver,
-            elementIds,
+            featureIds,
             amounts,
             ""
         );
