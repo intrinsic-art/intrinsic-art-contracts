14e2e50f7702a5289111371b17866d6907fe02fc
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-23 23:16:44 -0400

    updated mock element interface

--- contracts/interfaces/IMockElement.sol
+++ contracts/interfaces/IMockElement.sol
@@ -2,59 +2,21 @@
 pragma solidity ^0.8.0;
 
 interface IMockElement {
-  struct FeatureInfo {
-      string featureCategory;
-      uint256[] featureTokenIds;
-  }
+    function setURI(string memory newuri) external;
 
-  function setURI(string memory newuri) external;
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount
+    ) external;
 
-  function mint(
-    address account,
-    uint256 id,
-    uint256 amount,
-    bytes memory data
-  ) external;
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) external;
 
-  function mintBatch(
-    address to,
-    uint256[] memory ids,
-    uint256[] memory amounts,
-    bytes memory data
-  ) external;
-
-  function burn(
-    address account,
-    uint256 id,
-    uint256 value
-  ) external;
-
-  /**
-   * @dev See {IERC1155-safeBatchTransferFrom}.
-   */
-  function safeBatchTransferFrom(
-    address from,
-    address to,
-    uint256[] memory ids,
-    uint256[] memory amounts,
-    bytes memory data
-  ) external;
-
-  /// @notice Function for returning a project's feature info
-  function findProjectFeatureInfo(uint256 projectId) external view returns (FeatureInfo[] memory);
-
-  function findIdToCategory(uint256 projectId, uint256 featureId)
-    external
-    view
-    returns (string memory categoryString);
-
-  function findidsToFeatureAndCategories(
-    uint256[] memory featureIds,
-    uint256 projectId
-  )
-    external
-    view
-    returns (string[] memory features, string[] memory categories);
-
-  function totalSupply(uint256 id) external view returns (uint256);
+    function createFeature(string calldata feature)
+        external
+        returns (uint256 tokenId);
 }
