58f7e6f57ce6fdbdb657a761837a0f29b43561bc
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-08 22:46:33 -0400

    added struct for easy lookup and state view function

--- contracts/interfaces/IDutchAuction.sol
+++ contracts/interfaces/IDutchAuction.sol
@@ -2,56 +2,71 @@
 pragma solidity ^0.8.0;
 
 interface IDutchAuction {
-  struct Auction {
-    uint256 startTokenId;
-    uint256 endTokenId;
-    uint256 startTime;
-    uint256 endTime;
-    uint256 startPrice;
-    uint256 endPrice;
-    address artistAddress;
-    address erc721;
-    address currency;
-  }
+    struct Auction {
+        uint256 startTokenId;
+        uint256 endTokenId;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 startPrice;
+        uint256 endPrice;
+        address artistAddress;
+        address erc721;
+        address currency;
+    }
 
-  event AuctionAdded(
-    address indexed auctionCreator,
-    uint256 indexed projectId,
-    uint256 startTokenId,
-    uint256 endTokenId,
-    uint256 startTime,
-    uint256 endTime,
-    uint256 startPrice,
-    uint256 endPrice,
-    address indexed artistAddress
-  );
+    event AuctionAdded(
+        address indexed auctionCreator,
+        uint256 indexed projectId,
+        uint256 startTokenId,
+        uint256 endTokenId,
+        uint256 startTime,
+        uint256 endTime,
+        uint256 startPrice,
+        uint256 endPrice,
+        address indexed artistAddress
+    );
 
-  event CanvasesBought(
-    address indexed auctionCreator,
-    uint256 indexed projectId,
-    address indexed artistAddress,
-    address currency,
-    uint256 quantity,
-    uint256 canvasesTotalPrice
-  );
+    event CanvasesBought(
+        address indexed auctionCreator,
+        uint256 indexed projectId,
+        address indexed artistAddress,
+        address currency,
+        uint256 quantity,
+        uint256 canvasesTotalPrice
+    );
 
-  event ArtistClaimedRevenue(
-    address indexed recipient,
-    address indexed currency,
-    uint256 claimedRevenue
-  );
+    event ArtistClaimedRevenue(
+        address indexed recipient,
+        address indexed currency,
+        uint256 claimedRevenue
+    );
 
-  function addAuction(
-    uint _projectId,
-    Auction memory _auction
-  ) external;
+    function addAuction(uint256 _projectId, Auction memory _auction) external;
 
-  function buyCanvases(address _auctionCreator, uint256 _projectId, uint256 _quantity) external;
+    function buyCanvases(
+        address _auctionCreator,
+        uint256 _projectId,
+        uint256 _quantity
+    ) external;
 
-  function artistClaimRevenue(address _recipient, address _currency) external;
+    function artistClaimRevenue(address _recipient, address _currency) external;
 
-  function getCanvasPrice(address _auctionCreator, uint256 _projectId)
-    external
-    view
-    returns (uint256 canvasPrice);
+    function projectIdToAuction(address, uint256)
+        external
+        returns (
+            uint256 startTokenId,
+            uint256 endTokenId,
+            uint256 startTime,
+            uint256 endTime,
+            uint256 startPrice,
+            uint256 endPrice,
+            address artistAddres,
+            address erc721,
+            address currency
+        );
+
+    function getCanvasPrice(address _auctionCreator, uint256 _projectId)
+        external
+        view
+        returns (uint256 canvasPrice);
 }
