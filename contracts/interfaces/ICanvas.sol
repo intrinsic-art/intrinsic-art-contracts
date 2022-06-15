b3a00450e24666e4d72ba4a0be8afefe191a745a
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-14 22:02:45 -0400

    updated interface

--- contracts/interfaces/ICanvas.sol
+++ contracts/interfaces/ICanvas.sol
@@ -2,14 +2,37 @@
 pragma solidity ^0.8.0;
 
 interface ICanvas {
-    function initialize() external;
-
-    function addProject(
-        string memory _projectName,
-        address _artistAddress,
-        uint256 _maxInvocations,
-        bool _dynamic
+    function initialize(
+        address _element,
+        address _dutchAuction,
+        address _coloringBook
     ) external;
 
-    function safeMint(address to, uint256 _projectId) external returns (uint256 tokenId);
+    function safeMint(address _to, uint256 _projectId)
+        external
+        returns (uint256 tokenId);
+
+    function wrap(
+        address owner,
+        uint256[] memory featureIds,
+        uint256[] memory amounts,
+        uint256 canvasId
+    ) external;
+
+    function unWrap(
+        address receiver,
+        uint256[] memory featureIds,
+        uint256[] memory amounts,
+        uint256 canvasId
+    ) external;
+
+    /// @dev returns the category of a feature
+    /// @notice Should be used to assure different features
+    /// from the same category are not wrapped together
+    function findIdToCategory(uint256 projectId, uint256 featureId)
+        external
+        view
+        returns (string memory categoryString);
+
+    event MintedToken(address receiver, uint256 projectid, uint256 tokenId);
 }
