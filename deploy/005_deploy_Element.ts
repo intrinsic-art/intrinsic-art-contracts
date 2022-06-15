27c0a43cf3e6fb52a8f4e2e0dcb0b922f06f3c21
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-14 23:03:09 -0400

    deploy

--- deploy/005_deploy_Element.ts
+++ deploy/005_deploy_Element.ts
@@ -2,9 +2,11 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { deployContract } from "../helpers/deployContract";
 import Config from "../helpers/config";
+import { ethers } from "hardhat";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  await deployContract(hre, "Element", []);
+  const amm = await ethers.getContract("AMM");
+  await deployContract(hre, "Element", [amm.address]);
 };
 
 export default func;
