29c187f37f3b8ca9e0d7965adfe203fa603aab3f
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-21 17:17:38 -0400

    deploy weth if 31337

---
+++ deploy/006_deploy_MockWeth.ts
@@ -0,0 +1,11 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { deployContract } from "../helpers/deployContract";
+import { ethers } from "hardhat";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { chainId } = await ethers.provider.getNetwork();
+  if (chainId === 31337) await deployContract(hre, "MockWeth", []);
+};
+
+export default func;
