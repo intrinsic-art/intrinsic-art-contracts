775441db65538c7a5cd4193a26dc5f9cca7ac448
Christopher Dancy <chris.dancy@decentlabs.io>
2022-07-02 14:46:58 -0400

    create art function that buys and sell tokens based on curent ownershiip

--- contracts/Canvas.sol
+++ contracts/Canvas.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./Element.sol";
 import "./ColoringBook.sol";
+import "./AMM.sol";
 
 contract Canvas is
     ICanvas,
@@ -20,6 +21,7 @@ contract Canvas is
     Element public element;
     address public dutchAuction;
     ColoringBook public coloringBook;
+    AMM public amm;
     // TokenId Storage
     mapping(uint256 => uint256) public projectToInvocations;
     mapping(uint256 => uint256) public tokenIdToProjectId;
@@ -27,11 +29,6 @@ contract Canvas is
     mapping(uint256 => bytes32) public tokenIdTohash;
     mapping(bytes32 => uint256) public hashToTokenId;
     // Wrapping Storage
-    mapping(uint256 => mapping(uint256 => uint256))
-        public canvasIdToFeatureToBalances;
-    mapping(uint256 => uint256[]) public canvasIdToFeatures;
-    mapping(uint256 => mapping(uint256 => uint256))
-        public canvasIdToFeatureArrayIndex;
     mapping(uint256 => mapping(string => uint256))
         public canvasIdToCategoryToFeatureId;
 
@@ -47,13 +44,15 @@ contract Canvas is
     function initialize(
         address _element,
         address _dutchAuction,
-        address _coloringBook
+        address _coloringBook,
+        address _amm
     ) external initializer {
         __ERC721_init("Elements", "PROTON");
         __ERC721Burnable_init();
         element = Element(_element);
         dutchAuction = _dutchAuction;
         coloringBook = ColoringBook(_coloringBook);
+        amm = AMM(_amm);
     }
 
     function safeMint(address _to, uint256 _projectId)
@@ -90,97 +89,87 @@ contract Canvas is
     }
 
     ////////// Wrapping Functions ///////////
-    function wrap(
-        address owner,
-        uint256[] memory featureIds,
-        uint256[] memory amounts,
-        uint256 canvasId
-    ) public onlyOwner(canvasId) {
-        for (uint256 i; i < featureIds.length; i++) {
-            string memory featureCategory = findIdToCategory(
-                tokenIdToProjectId[canvasId],
-                featureIds[i]
-            );
-            // If the assigned feature == current feature then just update balance
-            if (
-                canvasIdToCategoryToFeatureId[canvasId][featureCategory] !=
-                featureIds[i]
-            ) {
-                // If there is a feature assigned to a category
-                // Move on to the next index, Do not wrap features w/ same category
-                if (
-                    canvasIdToCategoryToFeatureId[canvasId][featureCategory] > 0
-                ) {
-                    continue;
-                }
-
-                // Assign a feature to a category
-                canvasIdToCategoryToFeatureId[canvasId][
-                    featureCategory
-                ] = featureIds[i];
-
-                // Push the feature to the array of features/canvasId
-                canvasIdToFeatures[canvasId].push(featureIds[i]);
-
-                // Track the current index for easy lookup
-                canvasIdToFeatureArrayIndex[canvasId][featureIds[i]] =
-                    canvasIdToFeatures[canvasId].length -
-                    1;
-            }
-            // update balances of features
-            canvasIdToFeatureToBalances[canvasId][featureIds[i]] += amounts[i];
-        }
-        element.safeBatchTransferFrom(
-            owner,
-            address(this),
-            featureIds,
-            amounts,
-            ""
-        );
-        emit WrappedTokens(canvasId, featureIds, amounts);
-    }
-
-    function unWrap(
+    /// @dev includes:
+    function createArt(
         address receiver,
         uint256[] memory featureIds,
-        uint256[] memory amounts,
+        bool[] memory sell,
+        uint256[] memory minERC20ToReceive,
+        uint256 maxERC20ToSpend,
         uint256 canvasId
-    ) public onlyOwner(canvasId) {
-        for (uint256 i; i < featureIds.length; i++) {
-            // If there is not a balance/ a feature is not wrapped - continue
-            if (canvasIdToFeatureToBalances[canvasId][featureIds[i]] == 0) {
+    ) internal {
+        // state vars => memory
+        uint256 projectId = tokenIdToProjectId[canvasId];
+        (string[] memory categories, , ) = coloringBook
+            .findProjectCategoryAndFeatureStrings(projectId);
+        require(
+            featureIds.length == categories.length,
+            "Unequal features to categories"
+        );
+
+        for (uint256 i; i < categories.length; i++) {
+            // Skip if re-assigning same feature
+            if (
+                canvasIdToCategoryToFeatureId[canvasId][categories[i]] ==
+                featureIds[i]
+            ) {
                 continue;
             }
 
-            canvasIdToFeatureToBalances[canvasId][featureIds[i]] -= amounts[i];
-            // if the canvas balance == 0 then remove it
-            if (canvasIdToFeatureToBalances[canvasId][featureIds[i]] == 0) {
-                uint256 featureIndex = canvasIdToFeatureArrayIndex[canvasId][
-                    featureIds[i]
-                ];
-                uint256 arrayLength = canvasIdToFeatures[canvasId].length;
-                // Save the last index at the current index being removed
-                canvasIdToFeatures[canvasId][featureIndex] = canvasIdToFeatures[
-                    canvasId
-                ][arrayLength - 1];
-                canvasIdToFeatures[canvasId].pop();
+            require(
+                keccak256(abi.encodePacked(categories[i])) ==
+                    keccak256(
+                        (
+                            abi.encodePacked(
+                                coloringBook.projectIdToFeatureIdToCategory(
+                                    projectId,
+                                    featureIds[i]
+                                )
+                            )
+                        )
+                    ),
+                "Feature does not belong to category"
+            );
 
-                // assign the 0 index to the current category
-                string memory featureCategory = findIdToCategory(
-                    tokenIdToProjectId[canvasId],
-                    featureIds[i]
+            // Check if category is currently assined a feature
+            // unWrap and Wrap features
+            if (canvasIdToCategoryToFeatureId[canvasId][categories[i]] > 0) {
+                // token ids start > 0
+                _unWrap(
+                    categories[i],
+                    receiver,
+                    sell[i],
+                    featureIds[i],
+                    minERC20ToReceive[i],
+                    canvasId
                 );
-                canvasIdToCategoryToFeatureId[canvasId][featureCategory] = 0;
             }
+            _wrap(categories[i], featureIds[i], maxERC20ToSpend, canvasId);
         }
-        element.safeBatchTransferFrom(
-            address(this),
-            receiver,
-            featureIds,
-            amounts,
-            ""
-        );
-        emit UnWrappedTokens(canvasId, featureIds, amounts);
+    }
+
+    /// @notice unWrap underlying asset
+    function unWrap(
+        address receiver,
+        uint256[] memory featureIds,
+        bool[] memory sell,
+        uint256[] memory minERC20ToReceive,
+        uint256 canvasId
+    ) public onlyOwner(canvasId) {
+        for (uint256 i; i < featureIds.length; i++) {
+            string memory category = findIdToCategory(
+                tokenIdToProjectId[canvasId],
+                featureIds[i]
+            );
+            _unWrap(
+                category,
+                receiver,
+                sell[i],
+                featureIds[i],
+                minERC20ToReceive[i],
+                canvasId
+            );
+        }
     }
 
     /////// View Functions ///////////
@@ -223,5 +212,72 @@ contract Canvas is
         return
             interfaceId == type(IERC1155Receiver).interfaceId ||
             super.supportsInterface(interfaceId);
+    }
+
+    ///////////// Internal Function ///////////////
+    /// @notice This functions completes the following:
+    /// Check tokenId ownership
+    /// Purchases token if user does not own token
+    /// Wrap token - Within canvas
+    /// Assign to category
+    function _wrap(
+        string memory category,
+        uint256 featureId,
+        uint256 maxERC20ToSpend,
+        uint256 canvasId
+    ) internal {
+        if (
+            element.balanceOf(msg.sender, featureId) == 0 // check ownership
+        ) {
+            amm.buyElements( // Buy elements user does not
+                address(coloringBook),
+                featureId,
+                1,
+                maxERC20ToSpend,
+                msg.sender,
+                msg.sender
+            );
+        }
+
+        element.safeTransferFrom(msg.sender, address(this), featureId, 1, "");
+
+        // Assign a feature to a category
+        canvasIdToCategoryToFeatureId[canvasId][category] = featureId;
+        emit WrappedTokens(canvasId, featureId, 1);
+    }
+
+    /// @notice unWrap underlying asset
+    function _unWrap(
+        string memory category,
+        address _receiver,
+        bool _sell,
+        uint256 _featureId,
+        uint256 _minERC20ToReceive,
+        uint256 _canvasId
+    ) internal {
+        canvasIdToCategoryToFeatureId[_canvasId][
+            category // assign category => featureId = 0
+        ] = 0;
+        if (_sell) {
+            // sell elements
+            amm.sellElements(
+                address(coloringBook),
+                _featureId,
+                1,
+                _minERC20ToReceive,
+                _receiver,
+                address(this)
+            );
+        } else {
+            // or transfer elements
+            element.safeTransferFrom(
+                address(this),
+                _receiver,
+                _featureId,
+                1,
+                ""
+            );
+        }
+        emit UnWrappedTokens(_canvasId, _featureId, 1);
     }
 }
