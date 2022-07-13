0069f80370eed6fc192d479ae618a84027d715f9
tbwebb22 <tbwebb22@gmail.com>
2022-07-12 22:45:01 -0400

    Update ICanvas.sol

--- contracts/interfaces/ICanvas.sol
+++ contracts/interfaces/ICanvas.sol
@@ -2,24 +2,29 @@
 pragma solidity ^0.8.0;
 
 interface ICanvas {
-    function initialize(
-        address _element,
-        address _dutchAuction,
-        address _coloringBook,
-        address _amm
-    ) external;
+    struct ProjectData {
+        address studio;
+        address minter;
+        uint256 invocations;
+        uint256 maxInvocations;
+    }
 
-    function safeMint(address _to, uint256 _projectId)
+    function initialize() external;
+
+    function mint(uint256 _projectId, address _to)
         external
         returns (uint256 tokenId);
 
-    /// @dev returns the category of a feature
-    /// @notice Should be used to assure different features
-    /// from the same category are not wrapped together
-    function findIdToCategory(uint256 projectId, uint256 featureId)
+    function createProject(
+        address _studio,
+        address _minter,
+        uint256 _maxInvocations
+    ) external returns (uint256 projectId);
+
+    function getProjectIdFromCanvasId(uint256 canvasId)
         external
-        view
-        returns (string memory categoryString);
+        pure
+        returns (uint256 projectId);
 
     event MintedToken(address receiver, uint256 projectid, uint256 tokenId);
     event WrappedTokens(uint256 canvasId, uint256 tokenIds, uint256 amounts);
