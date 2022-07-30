f8ec03052420b42d475949fadccf1ba25006b4e4
tbwebb22 <tbwebb22@gmail.com>
2022-07-30 13:00:28 -0400

    Update config for Goerli deployments

--- helpers/config.ts
+++ helpers/config.ts
@@ -4,7 +4,8 @@ const config = {
   createProjectData: {
     name: "Disentanglement",
     description: "Relationships",
-    artistAddress: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+    // artistAddress: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", // localhost
+    artistAddress: "0x6bd62FeB486Bf699Ac04eD6DC09dE36D11720509", // Goerli
     artistName: "Phil Smith",
     website: "https://disentanglement.com",
     license: "MIT",
@@ -60,12 +61,15 @@ const config = {
     endTime: 1000000000000,
     startPrice: ethers.utils.parseEther("1"),
     endPrice: ethers.utils.parseEther("0.1"),
-    artistAddress: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
-    erc20Token: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+    // artistAddress: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8", // localhost
+    artistAddress: "0x6bd62FeB486Bf699Ac04eD6DC09dE36D11720509", // Goerli
+    // erc20Token: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",  // localhost
+    erc20Token: "0x4bAA305f6cfE512960356F8484f8a03898E3E140",
   },
   createAMMData: {
     startTime: 1000,
-    erc20Token: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+    // erc20Token: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707", // localhost
+    erc20Token: "0x4bAA305f6cfE512960356F8484f8a03898E3E140",
     constantA: [
       [
         ethers.utils.parseUnits("5", 16),
