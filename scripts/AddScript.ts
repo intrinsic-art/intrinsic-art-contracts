de008c9325d233c35c2ce7fd9e88e670d744ad01
tbwebb22 <tbwebb22@gmail.com>
2022-07-30 13:00:50 -0400

    Update gasLimit to match Goerli

--- scripts/AddScript.ts
+++ scripts/AddScript.ts
@@ -9,31 +9,31 @@ const AddScript = async (
   const studio = await hre.ethers.getContractAt("Studio", studioAddress);
 
   await studio.updateScript(projectId, 0, script[0], {
-    gasLimit: 30000000,
+    gasLimit: 8000000,
   });
 
   console.log("Added 0");
 
   await studio.updateScript(projectId, 1, script[1], {
-    gasLimit: 30000000,
+    gasLimit: 8000000,
   });
 
   console.log("Added 1");
 
   await studio.updateScript(projectId, 2, script[2], {
-    gasLimit: 30000000,
+    gasLimit: 8000000,
   });
 
   console.log("Added 2");
 
   await studio.updateScript(projectId, 3, script[3], {
-    gasLimit: 30000000,
+    gasLimit: 8000000,
   });
 
   console.log("Added 3");
 
   await studio.updateScript(projectId, 4, script[4], {
-    gasLimit: 30000000,
+    gasLimit: 8000000,
   });
 
   console.log("Added 4");
