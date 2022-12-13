865e8d46aca2f73d69dc7db43cfea3c7a0d52193
tbwebb22 <tbwebb22@gmail.com>
2022-12-12 20:52:38 -0500

    Update 005_protocol_setup.ts

--- deploy/005_protocol_setup.ts
+++ deploy/005_protocol_setup.ts
@@ -1,25 +1,34 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { deployContract } from "../helpers/deployContract";
 import { ethers } from "hardhat";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { chainId } = await ethers.provider.getNetwork();
+  let owner = "";
+  if (chainId === 1 && !!process.env.MAINNET_OWNER_ADDRESS) {
+    owner = process.env.MAINNET_OWNER_ADDRESS;
+  } else if (chainId === 5 && !!process.env.GOERLI_OWNER_ADDRESS) {
+    owner = process.env.GOERLI_OWNER_ADDRESS;
+  } else if (chainId === 11155111 && !!process.env.SEPOLIA_OWNER_ADDRESS) {
+    owner = process.env.SEPOLIA_OWNER_ADDRESS;
+  } else if (chainId === 80001 && !!process.env.POLYGON_MUMBAI_OWNER_ADDRESS) {
+    owner = process.env.POLYGON_MUMBAI_OWNER_ADDRESS;
+  } else {
+    console.error("Chain not configured");
+  }
   if (
     chainId === 31337 ||
     chainId === 5 ||
     chainId === 11155111 ||
     chainId === 80001
   ) {
-    await deployContract(hre, "MockWeth", []);
     const canvas = await hre.ethers.getContract("Canvas");
     const element = await hre.ethers.getContract("Element");
     const studio = await hre.ethers.getContract("Studio");
-    const mockWeth = await hre.ethers.getContract("MockWeth");
 
     await canvas.addStudio(studio.address);
     await element.addStudio(studio.address);
-    await studio.addApprovedERC20(mockWeth.address);
+    await studio.addAdmins([owner]);
   }
 };
 
