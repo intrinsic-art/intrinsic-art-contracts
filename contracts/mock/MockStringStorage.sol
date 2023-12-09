83c068db73e1f68b584d385e1f0ec43c66a9e47e
tbwebb22 <tbwebb22@gmail.com>
2023-12-09 18:12:50 -0500

    Single string in mock storage

--- contracts/mock/MockStringStorage.sol
+++ contracts/mock/MockStringStorage.sol
@@ -4,14 +4,15 @@ pragma solidity =0.8.19;
 import {IStringStorage} from "../interfaces/IStringStorage.sol";
 
 contract MockStringStorage is IStringStorage {
-  string constant private _stringSlot1 = "Test Script 1";
+  // strings stored in separate variables instead of array
+  // since solidity doesn't support constant string array variables
+  string constant private _stringSlot0 = "Test JSON";
+  string constant private _stringSlot1 ="Test Script";
 
   /** @inheritdoc IStringStorage*/
   function stringAtSlot(uint8 _slot) external pure returns (string memory) {
-    if (_slot == 0) {
-      return _stringSlot1;
-    }
-
+    if (_slot == 0) return _stringSlot0;
+    if (_slot == 1) return _stringSlot1;
     revert EmptySlot();
   }
 }
