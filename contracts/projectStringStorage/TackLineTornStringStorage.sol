430d0a2f1288764bb01d1be0db1aa05493031bea
tbwebb22 <tbwebb22@gmail.com>
2023-12-10 10:45:01 -0500

    Create TackLineTornStringStorage.sol

---
+++ contracts/projectStringStorage/TackLineTornStringStorage.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GNU GPLv3
+pragma solidity =0.8.19;
+
+import {IStringStorage} from "../interfaces/IStringStorage.sol";
+
+contract TackLineTornStringStorage is IStringStorage {
+  // strings stored in separate variables instead of array
+  // since solidity doesn't support constant string array variables
+  string constant private _stringSlot0 = "{\"name\": \"Tack Line Torn\",\"description\": \"Tack Line Torn is a generative art project that takes the viewer on a visual journey through the chaos of a tearing sail tack line. In sailing, a tack line is crucial to the stability and direction of a vessel, and in this project, the once-reliable line is portrayed as it breaks apart in real-time. The piece is a representation of the unpredictable nature of life, as the path of fragmentation is unique with each viewing. The fragmented line symbolizes the loss of control and the fragility of stability, and invites the viewer to contemplate the beauty that can be found in the midst of destruction. Tack Line Torn is an immersive and thought-provoking generative art project that explores the themes of unpredictability, transience, and the beauty of chaos.\",\"artistName\": \"Phil Smith\",\"website\": \"https://tacklinetorn.com\",\"license\": \"MIT\",\"scriptLibrary\": \"p5.js@1.0.0\",\"aspectRatio\": \"1:1\"}";
+  string constant private _stringSlot1 ="let seed,imageDimension,referenceRatio,referenceDimension=1e3;function setup(){seed=parseInt(artwork.seed.slice(0,16),16),referenceRatio=(imageDimension=Math.min(windowWidth,windowHeight))/referenceDimension,createCanvas(imageDimension,imageDimension),getArt()}function getArt(){let e,n,a,r,i,$,g,_,s;switch(artwork.traits.complexity){case\"minimal\":_=range(1,4);break;case\"balanced\":_=range(16,24);break;case\"complex\":_=range(40,48)}switch(colorMode(HSB,360,100,100,100),push(),artwork.traits.palette){case\"warm\":s=range(270,450)%360;break;case\"cool\":s=range(91,269);break;case\"mixed\":s=range(0,360)}background(s,rangeFloor(60,100),rangeFloor(80,100)),a=range(-1,1),r=range(-.5,.5),i=range(-1,1),$=range(-1,1),n=range(.05,.8)*imageDimension,g=0;for(let o=0;o<_;o++){switch(push(),artwork.traits.palette){case\"warm\":s=range(270,450)%360;break;case\"cool\":s=range(91,269);break;case\"mixed\":s=(s+range(140,220))%360}switch(stroke(e=color(s,100,100,15)),strokeWeight(.001*imageDimension),angleMode(DEGREES),artwork.traits.organization){case\"chaotic\":a=range(-1,1),r=range(-.5,.5),i=range(-1,1),$=range(-1,1),n=range(.05,.8)*imageDimension,g=range(0,.01);break;case\"ordered\":break;case\"emergent\":a=range(-1,1),r=range(-.5,.5),i=range(-1,1),$=range(-1,1),n=range(.05,.8)*imageDimension}translate(range(0,imageDimension),range(0,imageDimension));for(let t=0;t<2*referenceDimension;t++)push(),rotate(t*a*range(1-2*g,1+2*g)),line(0,0,0,n*range(1-g,1+g)),pop(),rotate(r),translate(i*referenceRatio*range(1-g,1+g),$*referenceRatio*range(1-g,1+g));pop()}return canvas.toDataURL()}function rnd(){return seed^=seed<<13,seed^=seed>>17,((seed^=seed<<5)<0?1+~seed:seed)%1e3/1e3}function range(e,n){return void 0===n&&(n=e,e=0),rnd()*(n-e)+e}function rangeFloor(e,n){return void 0===n&&(n=e,e=0),Math.floor(range(e,n))}";
+
+  /** @inheritdoc IStringStorage*/
+  function stringAtSlot(uint8 _slot) external pure returns (string memory) {
+    if (_slot == 0) return _stringSlot0;
+    if (_slot == 1) return _stringSlot1;
+    revert EmptySlot();
+  }
+}
